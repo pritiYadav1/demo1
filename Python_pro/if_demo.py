adc48f55ac940803df26aa5550d565a75734f2cf
pritiYadav1 <115352601+pritiYadav1@users.noreply.github.com>
2024-08-30 13:19:44 +0530

    Add files via upload

---
+++ Python_pro/if_demo.py
@@ -0,0 +1,5 @@
+#Write a program to accept a no from the user and find out the square if the no is positive
+number=int(input("Enter your number"))
+if number>0 :
+    square=number*number
+    print("The square of the number is =",square)
